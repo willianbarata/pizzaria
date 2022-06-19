04e45494ab72771c5d281bdfb70d8b9d9d36edf0
willianbarata <willianbarata@gmail.com>
2022-06-19 01:20:59 -0300

    Criptografando antes de gravar no banco

--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,4 +1,6 @@
 import prismaClient from '../../prisma'
+import { hash } from 'bcryptjs';
+
 
 interface UserRequest{
     name: string;
@@ -24,11 +26,13 @@ class CreateUserService{
         throw new Error("User already exists")
     }
 
+    const passwordHash = await hash(password, 8);
+
     const user = await prismaClient.user.create({
         data:{
             name: name,
             email: email,
-            password: password,
+            password: passwordHash,
         },
         select:{
             id: true,
