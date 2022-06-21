664281e847157182425f084d21cf5eb1d2846921
willianbarata <willianbarata@gmail.com>
2022-06-21 19:45:21 -0300

    Criando post produto com imagem

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,4 +1,6 @@
 import { Router, Request, Response } from "express";
+import multer from 'multer';
+
 import 'express-async-errors';
 import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from "./controllers/user/AuthUserController";
@@ -7,8 +9,11 @@ import { CreateCategoryController } from "./controllers/category/CreateCategoryC
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
 import { CreateProductController } from "./controllers/product/CreateProductController";
 import { isAuthenticated } from './middlewares/isAuthenticated';
+import uploadConfig from './config/multer';
 
 const router = Router();
+
+const upload = multer(uploadConfig.upload("./tmp"));
 
 //rotas users
 router.post('/users', new CreateUserController().handle)
@@ -23,6 +28,6 @@ router.post('/category' , isAuthenticated, new CreateCategoryController().handle
 router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 //rotas product
-router.post('/product', isAuthenticated, new CreateProductController().handle)
+router.post('/product', isAuthenticated, upload.single('file'),new CreateProductController().handle)
 
 export { router };
