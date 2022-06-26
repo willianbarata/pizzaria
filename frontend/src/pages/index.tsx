757d64ee0d41e06cbeaa91299785952482928937
willianbarata <willianbarata@gmail.com>
2022-06-26 16:03:15 -0300

    configurando sass

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,8 @@
+import styles  from '../../styles/Home.module.scss';
+
 export default function Home() {
   return (
-    <div >
+    <div className={styles.container} >
         <h1> Willian </h1>
     </div>
   )
