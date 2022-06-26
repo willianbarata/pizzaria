ca4830830429af52b74395e40324e500791cef89
willianbarata <willianbarata@gmail.com>
2022-06-26 11:53:51 -0300

    Enviando order (tirando rascunho)

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -13,6 +13,7 @@ import { CreateOrderController } from "./controllers/order/CreateOrderController
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
 import { AddItemController } from "./controllers/order/AddItemController";
 import { RemoveItemController } from "./controllers/order/RemoveItemController";
+import { SendOrderController } from "./controllers/order/SendOrderController";
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
 import uploadConfig from './config/multer';
@@ -45,5 +46,6 @@ router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 
 router.post('/order/add', isAuthenticated, new AddItemController().Handle)
 router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
+router.put('/order/send', isAuthenticated, new SendOrderController().handle)
 
 export { router };
